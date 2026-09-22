c609c40f20dab3afc86feced89cd737fd1dd6859
Charitha Perumalla <2025cs_charithaperumalla_b@nie.ac.in>
2026-09-22 15:21:27 +0530

    multiple posts

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Student(BaseModel):
+    rollno: int
+    name: str
+    grade: str
+
 app = FastAPI()
 @app.get("/")
 def home():
@@ -9,3 +16,15 @@ def about():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+@app.post("/create")
+def create_item():
+    return {"item": "created"}
+@app.get("/student/{rollno}")
+def get_student(rollno):
+    return {"Result":"Distinction", "rollno": rollno}
+@app.get("/Candidate/{rollno}")
+def get_Candidate(rollno: int): 
+    return {"Result":"Distinction","rollno": rollno, "Type":str(type(rollno))}
+@app.post("/items")
+def create_item(item: Student):
+    return {"rollno": item.rollno, "name": item.name, "grade": item.grade}
